64e740665bd47a4b9be0e37a257b46d466a019f5
KottaNiranjan <95270261+KottaNiranjan@users.noreply.github.com>
2023-03-07 10:30:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask,render_template,request
 import numpy as np
-import pickle
-from database import engine
 from sqlalchemy import text
 
 # helo=[
@@ -22,7 +20,7 @@ from sqlalchemy import text
 
 app=Flask(__name__)
 
-model = pickle.load(open('Kidney.pkl', 'rb'))
+
 
 
 
@@ -49,40 +47,40 @@ def ro():
 # @app.route("/login")
 # def log():
 #     return render_template("login.html")
-@app.route("/",methods=['POST'])
-def getvalue():
-    login=[]
-    try:
-        with engine.connect() as conn:
-            result = conn.execute(text("select * from login"))
-            for row in result.all():
-                login.append(row._asdict())
-    except:
-        login=[{'id': 1, 'username': 'niranjan', 'pass': 'Niranjan@8822', 'email': 'kottaniranjan8822@gmail.com'}, 
-        {'id': 2, 'username': 'mani', 'pass': 'mani@123', 'email': 'mani123@gmail.com'}, 
-        {'id': 3, 'username': 'yaseen', 'pass': 'yaseen@123', 'email': 'yaseen@gmail.com'}, 
-        {'id': 4, 'username': 'manoj', 'pass': 'manoj0311', 'email': 'manoj@gmail.com'}]
+# @app.route("/",methods=['POST'])
+# def getvalue():
+#     login=[]
+#     try:
+#         with engine.connect() as conn:
+#             result = conn.execute(text("select * from login"))
+#             for row in result.all():
+#                 login.append(row._asdict())
+#     except:
+#         login=[{'id': 1, 'username': 'niranjan', 'pass': 'Niranjan@8822', 'email': 'kottaniranjan8822@gmail.com'}, 
+#         {'id': 2, 'username': 'mani', 'pass': 'mani@123', 'email': 'mani123@gmail.com'}, 
+#         {'id': 3, 'username': 'yaseen', 'pass': 'yaseen@123', 'email': 'yaseen@gmail.com'}, 
+#         {'id': 4, 'username': 'manoj', 'pass': 'manoj0311', 'email': 'manoj@gmail.com'}]
     
 
-    us=request.form['user']
-    pa=request.form["pass"]
-    for i in login:
-        if us=='admin':
-            return render_template("administrator.html",login=login)
-        if i['username']==us:
-            if i['pass']==pa:
+#     us=request.form['user']
+#     pa=request.form["pass"]
+#     for i in login:
+#         if us=='admin':
+#             return render_template("administrator.html",login=login)
+# #         if i['username']==us:
+#             if i['pass']==pa:
                 # with engine.connect() as conn:
                 #     result = conn.execute(text("select * from login"))
                 #     login=[]
                 #     for row in result.all():
                 #         login.append(row._asdict())
-                return render_template("login.html",login=login)
-                # return render_template("administrator.html")
-            else:
-                return render_template("login.html",s="Invalid Login Details!!")
-        else:
-            return render_template("login.html",s="Invalid Login Details!!")
-    return render_template("administrator.html")
+#                 return render_template("login.html",login=login)
+#                 # return render_template("administrator.html")
+#             else:
+#                 return render_template("login.html",s="Invalid Login Details!!")
+#         else:
+#             return render_template("login.html",s="Invalid Login Details!!")
+#     return render_template("administrator.html")
 
 # @app.route("/",methods=["POST"])
 # def predict():
